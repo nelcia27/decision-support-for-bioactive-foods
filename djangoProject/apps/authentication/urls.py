44626c1c085dde6335cd4d69a43b4edebff5405c
NadiraX <57359646+NadiraX@users.noreply.github.com>
2021-01-22 15:14:27 +0100

    super_user_checker
    added get is_superuser

--- djangoProject/apps/authentication/urls.py
+++ djangoProject/apps/authentication/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path("authentication/change_password/", change_password, name="change_password"),
     path("authentication/sign_in/", sign_in, name="sign_in"),
     path("authentication/sign_out/", sign_out, name="sign_out"),
-    path("authentication/active_users/", active_users, name="active_users")
+    path("authentication/active_users/", active_users, name="active_users"),
+    path("authentication/super_user/", super_user, name="super_user")
 ]
